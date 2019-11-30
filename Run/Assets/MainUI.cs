b1227d8cdb67c0477db58555dde618ca4d7e5c90
ljun1118 <ljun1118@ks.ac.kr>
2019-11-30 15:53:33 +0900

    메뉴 UI 미완성

---
+++ Run/Assets/MainUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BTNType
+{
+    New,
+    Continue,
+    Option,
+    Sound,
+    Back,
+    Quit
+}
+public class MainUI : MonoBehaviour
+{
+    private bool showMenu;
+    public GameObject parts;
+
+    public void Start()
+    {
+        showMenu = false;
+    }
+    public void Update()
+    {
+        if (showMenu == false)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                parts.SetActive(true);
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                parts.SetActive(false);
+            }
+        }
+        
+    }
+
+
+}
