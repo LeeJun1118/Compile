a8daa256b42dc262e734dc61f9a8dd557ae303f8
ljun1118 <ljun1118@ks.ac.kr>
2019-12-12 10:46:33 +0900

    copyrightBy URL버튼

---
+++ Run/Assets/UrlText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrlText : MonoBehaviour
+{
+    string url = "https://github.com/LeeJun1118/Compile";
+    public void UrlBtn()
+    {
+        Application.OpenURL(url);
+     
+    }
+}
